8c1a2b25348423314086b2643f2cdc4786c31e3f
Camille Briand <camille.briand@efrei.net>
2020-02-03 19:40:52 +0100

    Configure Vue to work with firestore plugin

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import '@/registerServiceWorker';
 import router from '@/router';
 import store from '@/store';
 import vuetify from '@/plugins/vuetify';
+import { firestorePlugin } from 'vuefire';
 
 Vue.config.productionTip = false;
+
+Vue.use(firestorePlugin);
 
 new Vue({
   router,
