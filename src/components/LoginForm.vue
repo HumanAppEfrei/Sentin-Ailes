f1e45e8ff28ba3fc46350b8a473160588109d5dc
Camille Briand <camille.briand@efrei.net>
2020-02-03 19:36:13 +0100

    Create and make use of a LoginForm.vue component for Login.vue view

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <form>
+    <v-text-field outlined
+            type="email"
+            v-model="email"
+            name="email"
+            placeholder="Email" />
+
+    <v-text-field outlined
+            type="password"
+            v-model="password"
+            name="password"
+            placeholder="Mot de passe" />
+
+    <v-btn
+      color="primary"
+      @click="submitForm"
+    >Connexion
+    </v-btn>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log(`Handling connection for email "${this.email}" and password "${this.password}"`);
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,39 +1,15 @@
 <template>
-  <form method="POST" action="#" @submit.prevent="submitForm">
-    <label for="login-email-input">Adresse e-mail:</label>
-    <input  type="email"
-            v-model="email"
-            name="email"
-            id="login-email-input"
-            placeholder="Email" />
-
-    <label for="login-password-input">Mot de passe</label>
-    <input  type="password"
-            v-model="password"
-            name="password"
-            id="login-password-input"
-            placeholder="Mot de passe" />
-
-    <input type="submit" value="Connexion" />
-  </form>
+  <v-container>
+    <LoginForm />
+  </v-container>
 </template>
 
 <script>
+import LoginForm from '@/components/LoginForm.vue';
+
 export default {
-  data() {
-    return {
-      email: '',
-      password: '',
-    };
-  },
-  methods: {
-    submitForm() {
-      console.log(`Handling connection for email "${this.email}" and password "${this.password}"`);
-    },
+  components: {
+    LoginForm,
   },
 };
 </script>
-
-<style>
-
-</style>
