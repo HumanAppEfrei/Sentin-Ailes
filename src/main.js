f88dc85b1c452688d31659883c4ea2f532d2c7a6
Camille Briand <camille.briand@efrei.net>
2020-02-14 14:05:05 +0100

    Add Firebase & Firestore support to Vue app

--- src/main.js
+++ src/main.js
@@ -6,13 +6,21 @@ import store from '@/store';
 import vuetify from '@/plugins/vuetify';
 import { firestorePlugin } from 'vuefire';
 
+import * as fb from './firebaseConfig';
+
+let app;
+
+fb.auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      vuetify,
+      render: h => h(App),
+    }).$mount('#app');
+  }
+});
+
 Vue.config.productionTip = false;
 
 Vue.use(firestorePlugin);
-
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App),
-}).$mount('#app');
