1bfec0aa978307cff235925f1c026cb95e167789
Camille Briand <camille.briand@efrei.net>
2020-02-16 15:15:23 +0100

    Add temporary (ugly) layout to LoginForm component

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,36 +1,57 @@
 <template>
-  <form>
-    <v-text-field outlined
-            type="email"
-            v-model="email"
-            name="email"
-            placeholder="Email" />
+  <div>
+    <form @submit.prevent="submitForm" ref="loginForm">
+      <v-text-field outlined
+              type="email"
+              v-model="email"
+              name="email"
+              placeholder="Email" />
 
-    <v-text-field outlined
-            type="password"
-            v-model="password"
-            name="password"
-            placeholder="Mot de passe" />
+      <v-text-field outlined
+              type="password"
+              v-model="password"
+              name="password"
+              placeholder="Mot de passe" />
 
-    <v-btn
-      color="primary"
-      @click="submitForm"
-    >Connexion
-    </v-btn>
-  </form>
+      <v-btn
+        type="submit"
+        color="primary"
+      >Connexion
+      </v-btn>
+    </form>
+
+    <v-alert type="error" class="mt-4" outlined v-show="loginError">
+      {{ loginError }}
+    </v-alert>
+
+    <v-alert type="success" class="mt-4" v-show="!loginError && loginData">
+      {{ JSON.stringify(loginData) }}
+    </v-alert>
+  </div>
 </template>
 
 <script>
+import firebase from 'firebase';
+
 export default {
   data() {
     return {
       email: '',
       password: '',
+      loginError: '',
+      loginData: null,
     };
   },
   methods: {
-    submitForm() {
+    async submitForm() {
       console.log(`Handling connection for email "${this.email}" and password "${this.password}"`);
+      this.$refs.loginForm.reset();
+      try {
+        this.loginData = await firebase.auth().signInWithEmailAndPassword(this.email, this.password);
+        // TODO: redirect users to their dashboard
+      } catch (err) {
+        this.loginError = err.message;
+      }
     },
   },
 };
