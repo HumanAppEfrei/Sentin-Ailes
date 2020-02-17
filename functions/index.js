d6c1329ed3c418ba3b9234d76bceec7f0d1332f2
Camille Briand <camille.briand@efrei.net>
2020-02-17 16:49:32 +0100

    Fix cloud function not using 'data' function but 'data' property

--- functions/index.js
+++ functions/index.js
@@ -23,31 +23,38 @@ const getUserWhitelistRecord = async email => {
 };
 
 const createUserFirestoreRecord = (uid, type) => {
-  userCollection.doc(uid).set({ type });
+  userCollection.doc(uid).set({ type }, { merge: true });
 };
 
 exports.onUserCreation = functions.auth.user().onCreate(async user => {
+  console.log("Processing user data with email:", user.email);
+
   // Check whether or not the user's email address is registered in the whitelist
   const userWhitelistRecord = await getUserWhitelistRecord(user.email);
   if (userWhitelistRecord === null) {
+    console.log("Unable to find whitelist record with provided email, deleting user...");
+
     // If email not in the whitelist, delete the newly created user record
     admin.auth().deleteUser(user.uid);
     return;
   }
 
+
   // Create Firestore record in 'users' collection with matching UID
   if (
     ['beneficiaire', 'intervenant', 'admin', 'superAdmin'].indexOf(
-      userWhitelistRecord.data.userType
+      userWhitelistRecord.data().userType
     ) !== -1
   ) {
-    createUserFirestoreRecord(user.uid, userWhitelistRecord.data.userType);
+    createUserFirestoreRecord(user.uid, userWhitelistRecord.data().userType);
   } else {
+    console.log("Unable to find valid userType value for whitelist record, deleting user...");
     // If whitelist record is invalid, delete the newly created user
     admin.auth().deleteUser(user.uid);
     return;
   }
 
   // Delete record from 'whitelist' collection
+  console.log("User created, removing whitelist record with provided email");
   whitelist.doc(userWhitelistRecord.id).delete();
 });
