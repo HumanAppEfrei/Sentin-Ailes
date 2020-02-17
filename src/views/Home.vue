5721d2d9e264a2110296065c08cd575b905f9837
Camille Briand <camille.briand@efrei.net>
2020-02-17 17:37:13 +0100

    Add sample card to home page to display logged in user

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,12 +5,27 @@
         Go to login page
       </v-btn>
     </router-link>
+
+    <v-container v-if="user">
+      <v-card>
+        <v-card-title>Connected User</v-card-title>
+        <v-card-subtitle>{{ user.auth.email }}</v-card-subtitle>
+        <v-card-subtitle>{{ user.auth.uid }}</v-card-subtitle>
+      </v-card>
+    </v-container>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
   name: 'home',
   components: {},
+  computed: {
+    ...mapState([
+      'user',
+    ]),
+  },
 };
 </script>
