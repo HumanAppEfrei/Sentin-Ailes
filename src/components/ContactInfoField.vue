605e86c1ebb67d9f897c55e4e5ec1bdb704351dc
Thomas LINTANF <thomas.lintanf@efrei.fr>
2020-03-27 20:15:47 +0100

    Created ContactInfoField component

---
+++ src/components/ContactInfoField.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <label>{{ fieldName }} : </label>
+    <input type='text' :value='fieldValue' :disabled='!editMode' />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fieldValue: 'PlaceHolder',
+    };
+  },
+  props: [
+    'fieldName',
+    'editMode',
+  ],
+};
+</script>
+
+<style lang='scss' scoped>
+</style>
