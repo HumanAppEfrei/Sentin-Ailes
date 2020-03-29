f0ca1ad753ae609c9f0c30dadf1a4fd9e55f0cc7
Thomas LINTANF <thomas.lintanf@efrei.fr>
2020-03-29 20:47:44 +0200

    Updated contact-info feature: - Added emergencyContact component.

---
+++ src/components/EmergencyContact.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-container>
+    <v-row justify="space-between">
+      <h2>Contact {{ contact.number }}</h2>
+      <v-btn @click="onDelete(contact.number)" v-if="editMode">Suprimer</v-btn>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-text-field outlined
+          type="text"
+          label="Prénom"
+          v-model="contact.firstName"
+          :readonly="!editMode"
+          prepend-inner-icon="person" />
+      </v-col>
+      <v-col>
+        <v-text-field outlined
+        type="text"
+        label="Nom"
+        v-model="contact.lastName"
+        :readonly="!editMode"
+        prepend-inner-icon="person" />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="8">
+        <v-text-field outlined
+          type="email"
+          label="Email"
+          v-model="contact.email"
+          :readonly="!editMode"
+          prepend-inner-icon="email" />
+      </v-col>
+      <v-col cols="12" sm="4">
+        <v-text-field outlined
+        label="Téléphone"
+        v-model="contact.phone"
+        :readonly="!editMode"
+        prepend-inner-icon="phone" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: [
+    'contact',
+    'editMode',
+    'onDelete',
+  ],
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/ContactInfo.vue
+++ src/views/ContactInfo.vue
@@ -1,139 +1,159 @@
 <template>
-<v-form>
-  <v-container >
-      <v-row justify="space-between">
-        <v-col cols="12" sm="7" order="1">
-          <h1>Informations personnelles</h1>
-        </v-col>
-        <v-col cols="12" sm="5" order="-1" order-sm="2">
-          <v-row justify="end" v-if="!editMode">
-            <v-col>
-              <v-spacer></v-spacer>
-            </v-col>
-            <v-col>
-              <v-btn @click="onEdit"><v-icon left>edit</v-icon> Modifier</v-btn>
-            </v-col>
-          </v-row>
-          <v-row justify="end" v-else>
-            <v-col>
-              <v-btn @click="onSave"><v-icon left>save_alt</v-icon>Enregistrer</v-btn>
-            </v-col>
-            <v-col>
-              <v-btn @click="onCancel"><v-icon left>cancel</v-icon>Annuler</v-btn>
-            </v-col>
-          </v-row>
-        </v-col>
-      </v-row>
-      <h2>Identitée</h2>
-      <v-row>
-        <v-col cols="12" sm="6">
-          <v-text-field outlined
-              type="text"
-              label="Prénon"
-              v-model="firstName"
-              :readonly="!editMode"
-              prepend-inner-icon="person"/>
-        </v-col>
-        <v-col cols="12" sm="6">
-          <v-text-field outlined
-              type="text"
-              label="Nom"
-              v-model="lastName"
-              :readonly="!editMode"
-              prepend-inner-icon="person"/>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="12" sm="4">
-          <v-menu
-            v-model="birthDateMenu"
-            :close-on-content-click="false"
-            transition="scale-transition"
-            offset-y
-            min-width="290px"
-            :disabled="!editMode">
-            <template v-slot:activator="{ on }">
-              <v-text-field
-                v-model="birthDate"
-                label="Date de naissance"
-                prepend-inner-icon="cake"
-                readonly
-                v-on="on"
-                outlined />
-            </template>
-            <v-date-picker v-model="birthDate" first-day-of-week="1" min="1900-01-01" @input="birthDateMenu = false" locale="fr"></v-date-picker>
-          </v-menu>
-        </v-col>
-        <v-col cols="12" sm="4">
-          <v-select
-            v-model="familySituation"
-            label="Situation familiale"
-            :items="familySituationOptions"
-            item-text="text"
-            item-value="value"
-            prepend-inner-icon="people_outline"
-            :readonly="!editMode"
-            outlined/>
+<v-container >
+  <v-form>
+    <v-row justify="space-between">
+      <v-col cols="12" sm="7" order="1">
+        <h1>Informations personnelles</h1>
       </v-col>
-      </v-row>
-      <h2>Contact</h2>
-      <v-row>
-        <v-col>
-          <v-text-field outlined
+      <v-col cols="12" sm="5" order="-1" order-sm="2">
+        <v-row justify="end" v-if="!editMode">
+          <v-col>
+            <v-spacer></v-spacer>
+          </v-col>
+          <v-col>
+            <v-btn @click="onEdit"><v-icon left>edit</v-icon> Modifier</v-btn>
+          </v-col>
+        </v-row>
+        <v-row justify="end" v-else>
+          <v-col>
+            <v-btn @click="onSave"><v-icon left>save_alt</v-icon>Enregistrer</v-btn>
+          </v-col>
+          <v-col>
+            <v-btn @click="onCancel"><v-icon left>cancel</v-icon>Annuler</v-btn>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+    <h2>Identitée</h2>
+    <v-row>
+      <v-col cols="12" sm="6">
+        <v-text-field outlined
             type="text"
-            label="Adresse"
-            v-model="address"
+            label="Prénon"
+            v-model="firstName"
             :readonly="!editMode"
-            prepend-inner-icon="place"
-             />
-        </v-col>
-      </v-row>
-
-      <v-row>
-        <v-col cols="12" sm="8">
-          <v-text-field outlined
+            prepend-inner-icon="person"/>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-text-field outlined
             type="text"
-            label="Ville"
-            v-model="city"
+            label="Nom"
+            v-model="lastName"
             :readonly="!editMode"
-            prepend-inner-icon="location_city" />
-        </v-col>
-        <v-col cols="12" sm="4">
-          <v-text-field outlined
-            type="text"
-            label="Code postal"
-            v-model="postalCode"
-            :readonly="!editMode"
-            prepend-inner-icon="map" />
-        </v-col>
-      </v-row>
-
-      <v-row>
-        <v-col cols="12" sm="8">
-          <v-text-field outlined
-          type="email"
-          label="Email"
-          v-model="email"
+            prepend-inner-icon="person"/>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="4">
+        <v-menu
+          v-model="birthDateMenu"
+          :close-on-content-click="false"
+          transition="scale-transition"
+          offset-y
+          min-width="290px"
+          :disabled="!editMode">
+          <template v-slot:activator="{ on }">
+            <v-text-field
+              v-model="birthDate"
+              label="Date de naissance"
+              prepend-inner-icon="cake"
+              readonly
+              v-on="on"
+              outlined />
+          </template>
+          <v-date-picker v-model="birthDate" first-day-of-week="1" min="1900-01-01" @input="birthDateMenu = false" locale="fr"></v-date-picker>
+        </v-menu>
+      </v-col>
+      <v-col cols="12" sm="4">
+        <v-select
+          v-model="familySituation"
+          label="Situation familiale"
+          :items="familySituationOptions"
+          item-text="text"
+          item-value="value"
+          prepend-inner-icon="people_outline"
           :readonly="!editMode"
-          prepend-inner-icon="email"
-          />
-        </v-col>
-        <v-col cols="12" sm="4">
-          <v-text-field outlined
-          type="phone"
-          label="Téléphone"
-          v-model="phone"
+          outlined/>
+    </v-col>
+    </v-row>
+    <h2>Contact</h2>
+    <v-row>
+      <v-col>
+        <v-text-field outlined
+          type="text"
+          label="Adresse"
+          v-model="address"
           :readonly="!editMode"
-          prepend-inner-icon="phone"
-          />
-        </v-col>
-      </v-row>
+          prepend-inner-icon="place"
+            />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12" sm="8">
+        <v-text-field outlined
+          type="text"
+          label="Ville"
+          v-model="city"
+          :readonly="!editMode"
+          prepend-inner-icon="location_city" />
+      </v-col>
+      <v-col cols="12" sm="4">
+        <v-text-field outlined
+          type="text"
+          label="Code postal"
+          v-model="postalCode"
+          :readonly="!editMode"
+          prepend-inner-icon="map" />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12" sm="8">
+        <v-text-field outlined
+        type="email"
+        label="Email"
+        v-model="email"
+        :readonly="!editMode"
+        prepend-inner-icon="email" />
+      </v-col>
+      <v-col cols="12" sm="4">
+        <v-text-field outlined
+        type="phone"
+        label="Téléphone"
+        v-model="phone"
+        :readonly="!editMode"
+        prepend-inner-icon="phone" />
+      </v-col>
+    </v-row>
+    <v-row>
       <h1>Contacts d'urgences</h1>
-  </v-container>
-</v-form>
+    </v-row>
+    <emergency-contact
+      v-for="contact in emergencyContacts"
+      :key="contact.number"
+      :contact="contact"
+      :edit-mode="editMode"
+      :on-delete="onDeleteContact">
+    </emergency-contact>
+    <v-row justify="center">
+      <v-btn v-if="editMode" @click="onAddContact">Ajouter un contact</v-btn>
+    </v-row>
+  </v-form>
+</v-container>
 </template>
 
 <script>
+import EmergencyContact from '@/components/EmergencyContact.vue';
+
+const contact1 = {
+  number: 1,
+  firstName: 'Jean',
+  lastName: 'Paul',
+  phone: '0102030405',
+  email: 'jean.paul@www.zzz',
+};
+
 export default {
   name: 'contact-info',
   data() {
@@ -149,6 +169,7 @@ export default {
       address: '1 rue de la paix',
       postalCode: '12345',
       city: 'Villejuif',
+      emergencyContacts: [contact1],
       familySituationOptions: [
         { text: 'Célibataire', value: 'single' },
         { text: 'Marié(e)', value: 'married' },
@@ -156,8 +177,9 @@ export default {
       ],
     };
   },
-  components: {},
-
+  components: {
+    EmergencyContact,
+  },
   methods: {
     onEdit() {
       this.editMode = true;
@@ -169,6 +191,24 @@ export default {
     onCancel() {
       /* Reload User data From db */
       this.editMode = false;
+    },
+    onAddContact() {
+      this.emergencyContacts.push(
+        {
+          number: this.emergencyContacts.length + 1,
+          firstName: '',
+          lastName: '',
+          phone: '',
+          email: '',
+        },
+      );
+    },
+    onDeleteContact(number) {
+      const array = this.emergencyContacts;
+      array.splice(number - 1, 1);
+      for (let i = number - 1; i < array.length; i += 1) {
+        array[i].number -= 1;
+      }
     },
   },
 };
