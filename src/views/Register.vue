49bb2324c6158b6b2c933de6b5ac3c2f6dfe1887
Thibault LEPEZ <thibault.lepez@efrei.net>
2020-04-01 19:58:17 +0200

    Add a stepper for the registration

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,16 @@
 <template>
-  <BeneficiaireRegisterForm />
+  <v-stepper v-model="e1" vertical>
+    <v-stepper-step :complete="e1 > 1" step="1">Type d'inscription</v-stepper-step>
+    <v-stepper-content step="1">
+      <!-- TODO: make the beneficiaire/intervenant(/admin?) form -->
+      <v-btn color="primary" @click="e1 = 2">Continue</v-btn>
+    </v-stepper-content>
+
+    <v-stepper-step :complete="e1 > 2" step="2">Inscription</v-stepper-step>
+    <v-stepper-content step="2">
+      <BeneficiaireRegisterForm v-if="formType === 'beneficiaire'"/>
+    </v-stepper-content>
+  </v-stepper>
 </template>
 
 <script>
@@ -12,6 +23,13 @@ export default {
   },
 
   methods: {
+  },
+
+  data() {
+    return {
+      e1: 1,
+      formType: 'beneficiaire',
+    };
   },
 };
 </script>
