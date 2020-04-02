af497c68251ca28a59123ff3e06e5189bc801e03
Camille Briand <camille.briand@efrei.net>
2020-04-02 16:40:12 +0200

    Implement display and deletion of whitelist records

--- src/views/Whitelist.vue
+++ src/views/Whitelist.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>
+  <v-container>
     <h1>Whitelist (authorized emails and associated roles)</h1>
 
     <v-data-table
       :headers="tableHeaders"
-      :items="whitelistEntries"
+      :items="whitelistRecords"
+      multi-sort
     >
+      <template v-slot:item.btn="{ item }">
+        <v-btn color="primary" @click="deleteItem(item)">
+          Delete
+        </v-btn>
+      </template>
     </v-data-table>
-  </div>
+  </v-container>
 </template>
 
 <script>
@@ -18,30 +24,52 @@ export default {
 
   data() {
     return {
+      unsubscribeFromSubscription: null,
       tableHeaders: [
         {
           text: 'Email',
-          value: 'email',
+          value: 'data.email',
           sortable: true,
         },
         {
           text: 'Role',
-          value: 'userType',
+          value: 'data.userType',
           sortable: true,
+        },
+        {
+          text: '',
+          value: 'btn',
+          sortable: false,
+          align: 'center',
         },
       ],
       whitelistRecords: [],
     };
   },
 
-  computed: {
-    whitelistEntries() {
-      return this.whitelistRecords.map(record => record.data);
+  methods: {
+    initItemDeletion(item) {
+      console.log(item);
+    },
+
+    async deleteItem(item) {
+      await item.ref.delete();
     },
   },
 
   async beforeMount() {
+    // Gather all whitelist records from database
     this.whitelistRecords = (await whitelistCollection.get()).docs.map(doc => ({ ref: doc.ref, data: doc.data() }));
+
+    // Subscribe to changes (and save unsubcription function)
+    this.unsubscribeFromSubscription = whitelistCollection.onSnapshot((snapshot) => {
+      this.whitelistRecords = snapshot.docs.map(doc => ({ ref: doc.ref, data: doc.data() }));
+    });
+  },
+
+  async beforeDestroy() {
+    // Unsubscribe from subscription
+    this.unsubscribeFromSubscription();
   },
 };
 </script>
