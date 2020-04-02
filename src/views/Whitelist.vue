b2bf24d9021782781608d2d74bc91b8b725a532a
Camille Briand <camille.briand@efrei.net>
2020-04-02 15:58:28 +0200

    Add default table view

--- src/views/Whitelist.vue
+++ src/views/Whitelist.vue
@@ -1,9 +1,47 @@
 <template>
-  <div></div>
+  <div>
+    <h1>Whitelist (authorized emails and associated roles)</h1>
+
+    <v-data-table
+      :headers="tableHeaders"
+      :items="whitelistEntries"
+    >
+    </v-data-table>
+  </div>
 </template>
 
 <script>
-export default {
+import { whitelistCollection } from '@/firebaseConfig';
 
+export default {
+  name: 'WhitelistView',
+
+  data() {
+    return {
+      tableHeaders: [
+        {
+          text: 'Email',
+          value: 'email',
+          sortable: true,
+        },
+        {
+          text: 'Role',
+          value: 'userType',
+          sortable: true,
+        },
+      ],
+      whitelistRecords: [],
+    };
+  },
+
+  computed: {
+    whitelistEntries() {
+      return this.whitelistRecords.map(record => record.data);
+    },
+  },
+
+  async beforeMount() {
+    this.whitelistRecords = (await whitelistCollection.get()).docs.map(doc => ({ ref: doc.ref, data: doc.data() }));
+  },
 };
 </script>
