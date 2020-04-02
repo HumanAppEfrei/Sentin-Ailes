d94b06175c3d3c967c8d99a8691438908970435b
Thibault LEPEZ <thibault.lepez@efrei.net>
2020-04-02 21:08:14 +0200

    Make appbar with icon specific to route

--- src/components/Appbar.vue
+++ src/components/Appbar.vue
@@ -1,9 +1,19 @@
 <template>
-  
+  <v-app-bar color="green darken-4">
+    <v-app-bar-nav-icon v-if="this.$route.name !== 'home'">
+      <v-icon v-on:click="back">arrow_back</v-icon>
+    </v-app-bar-nav-icon>
+    <v-toolbar-title class="white--text">Sentin'Ailes</v-toolbar-title>
+  </v-app-bar>
 </template>
 
 <script>
 export default {
-
-}
+  name: 'Appbar',
+  methods: {
+    back() {
+      this.$router.back();
+    },
+  },
+};
 </script>
