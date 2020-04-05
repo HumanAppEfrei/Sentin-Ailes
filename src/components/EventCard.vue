cb444b9b01ac8156c02418d99d8902d97032cbbc
Thomas LINTANF <thomas.lintanf@efrei.fr>
2020-04-05 05:03:54 +0200

    Created EventCard component

---
+++ src/components/EventCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-card height="150px">
+      <v-card-title>{{ event.name }}</v-card-title>
+      <v-card-text>{{ event.description }} </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      eventName: 'Event Name',
+    };
+  },
+  props: [
+    'event',
+  ],
+};
+</script>
+
+<style scoped>
+
+
+</style>
--- src/views/EventsList.vue
+++ src/views/EventsList.vue
@@ -3,23 +3,38 @@
     <h1>Consulter mes évenement programmés</h1>
   <v-row>
     <v-col class="flex-grow-0">
-      <v-date-picker v-model="selectedDate"></v-date-picker>
+      <v-date-picker v-model="selectedDate" fixed></v-date-picker>
     </v-col>
-    <v-col>
-      <v-row class="flex-column align-center">
-        <h2>Dans la semaine</h2>
-      </v-row>
+    <v-col class="flex-column align-center">
+      <h2 class="text-center">Dans la semaine</h2>
+      <event-card v-for="event in events" :key="event.id" :event="event"></event-card>
     </v-col>
   </v-row>
   </v-container>
 </template>
 
 <script>
+import EventCard from '../components/EventCard.vue';
+
+const eventDemo = {
+  name: 'Demo event',
+  date: '2020-04-01',
+  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+};
+
 export default {
   data() {
     return {
       selectedDate: '',
+      events: [
+        eventDemo,
+        eventDemo,
+        eventDemo,
+      ],
     };
+  },
+  components: {
+    EventCard,
   },
 };
 </script>
