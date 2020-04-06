b1b36d6288d761707e51e73b2e01468fee1233fa
R-Nbt <rubenabet@gmail.com>
2020-04-06 13:04:35 +0200

    Add hub component and hub page

---
+++ src/components/MenuTile.vue
@@ -0,0 +1,25 @@
+<template>
+  <router-link :to="link">
+    <v-card dark :color="color" hover>
+      <v-card-title class="justify-center">
+        <v-icon large>{{ icon }}</v-icon>
+      </v-card-title>
+      <v-card-text class="text-center display-1 font-weight-medium white--text">{{ text }}</v-card-text>
+    </v-card>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: [
+    'color',
+    'icon',
+    'text',
+    'link',
+  ],
+};
+</script>
+
+<style lang="css">
+  a {  text-decoration: none;}
+</style>
---
+++ src/views/Hub.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container>
+      <v-row>
+          <v-col cols="12" sm="6">
+            <MenuTile color="red" icon="home" text="yeet" link="/"/>
+          </v-col>
+
+          <v-spacer></v-spacer>
+
+          <v-col cols="12" sm="6">
+            <MenuTile/>
+          </v-col>
+      </v-row>
+
+      <v-row>
+          <v-col cols="12" sm="6">
+            <MenuTile/>
+          </v-col>
+
+          <v-spacer></v-spacer>
+
+          <v-col cols="12" sm="6">
+            <MenuTile/>
+          </v-col>
+      </v-row>
+  </v-container>
+</template>
+
+<script>
+import MenuTile from '@/components/MenuTile.vue';
+
+export default {
+  components: {
+    MenuTile,
+  },
+};
+</script>
+
+<style>
+
+</style>
