333ee4f8c88aeb73288f0cb6d960e9f0fe3c6834
R-Nbt <rubenabet@gmail.com>
2020-04-06 15:53:29 +0200

    Update component to include notifications support

--- src/components/MenuTile.vue
+++ src/components/MenuTile.vue
@@ -1,10 +1,16 @@
 <template>
   <router-link :to="link">
-    <v-card min-height=225 dark :color="color" hover>
+    <v-card align="center" min-height=225 dark :color="color" hover>
       <v-card-title class="justify-center">
-        <v-icon size="75">{{ icon }}</v-icon>
+          <v-icon size="75">{{ icon }}</v-icon>
       </v-card-title>
       <v-card-text class="text-center display-1 font-weight-medium white--text">{{ text }}</v-card-text>
+      <div v-if="notif == 1">
+        <v-chip color="red" ripple=false label="Notification">{{ notif }} notification</v-chip>
+      </div>
+      <div v-else-if="notif > 1">
+        <v-chip color="red" ripple=false label="Notification">{{ notif }} notifications</v-chip>
+      </div>
     </v-card>
   </router-link>
 </template>
@@ -16,10 +22,14 @@ export default {
     'icon',
     'text',
     'link',
+    'notif',
   ],
 };
 </script>
 
 <style lang="css">
-  a {  text-decoration: none;}
+  a {  text-decoration: none; }
+  span .v-chip__content {
+    font-size: 20px;
+  }
 </style>
