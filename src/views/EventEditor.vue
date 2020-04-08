888568512536ef31e6481f159ffc30e348971402
Thomas LINTANF <thomas.lintanf@efrei.net>
2020-04-08 21:17:41 +0200

    Rename btn

--- src/views/EventEditor.vue
+++ src/views/EventEditor.vue
@@ -1,20 +1,22 @@
 <template>
   <div>
-    <h1></h1>
+    <h1>Ajouter un event</h1>
     <v-row class="main">
     <v-col class="flex-grow-0">
       <v-date-picker v-model="date" />
     </v-col>
     <v-col class="d-flex flex-column">
-      <v-text-field outlined
-      v-model="destinataires"
-      class="flex-grow-0"
-      label="Participant(s)" />
-      <v-textarea outlined
-      v-model="message"
-      class="flex-grow-0"
-      label="Message" />
-      <v-btn class="align-self-center" color="primary">Envoyer</v-btn>
+      <v-form>
+        <v-text-field outlined
+        v-model="destinataires"
+        class="flex-grow-0"
+        label="Participant(s)" />
+        <v-textarea outlined
+        v-model="message"
+        class="flex-grow-0"
+        label="Message" />
+        <v-btn class="align-self-center" color="primary">Valider</v-btn>
+      </v-form>
     </v-col>
   </v-row>
   </div>
@@ -26,6 +28,7 @@ export default {
   name: 'EventEditor',
   data() {
     return {
+      isNew: true,
       date: '',
       destinataires: '',
       message: '',
