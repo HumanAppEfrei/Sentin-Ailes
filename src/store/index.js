fe12b45ac8349491a8ca50bc2258a9e449fa0d50
Thibault LEPEZ <thibault.lepez@efrei.net>
2020-04-11 21:24:14 +0200

    comment plugin

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable import/no-cycle */
 import Vue from 'vue';
 import Vuex from 'vuex';
+// import createPersistedState from 'vuex-persistedstate';
 
 import authModule from './modules/auth';
 import whitelistModule from './modules/whitelist';
@@ -7,6 +9,7 @@ import whitelistModule from './modules/whitelist';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  // plugins: [createPersistedState()],
   modules: {
     auth: authModule,
     whitelist: whitelistModule,
