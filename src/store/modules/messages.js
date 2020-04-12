1fef648ac525bfad89de45364af480bd1d5335fe
Camille Briand <camille.briand@efrei.net>
2020-04-12 16:05:44 +0200

    Create message.js module in store

---
+++ src/store/modules/messages.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-shadow */
+
+const state = {
+
+};
+
+const getters = {
+
+};
+
+const mutations = {
+
+};
+
+const actions = {
+
+};
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions,
+  namespaced: true,
+};
