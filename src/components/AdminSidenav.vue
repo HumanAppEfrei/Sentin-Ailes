a26ca67baf6dbe23d80345cb1f889a4289b44a74
Camille Briand <camille.briand@efrei.net>
2020-04-27 17:30:14 +0200

    Refactor admin navigation drawer to own AdminSidenav component

---
+++ src/components/AdminSidenav.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-navigation-drawer
+      color="primary"
+      permanent
+    >
+    <v-list>
+      <v-list-item to="/admin/whitelist">
+        <v-list-item-icon>
+          <v-icon class="white--text">mdi-account-details</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content class="white--text">
+          Whitelist
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'AdminSidenav',
+};
+</script>
+
+<style>
+
+</style>
--- src/views/AdminPanel.vue
+++ src/views/AdminPanel.vue
@@ -1,12 +1,30 @@
 <template>
-  <div>
-    <h1>Admin Panel</h1>
-    <router-view></router-view>
+  <div class="admin-panel p0 d-flex">
+    <AdminSidenav />
+    <v-container class="admin-panel-content">
+      <router-view></router-view>
+    </v-container>
   </div>
 </template>
 
 <script>
+import AdminSidenav from '@/components/AdminSidenav.vue';
+
 export default {
   name: 'AdminPanel',
+
+  components: {
+    AdminSidenav,
+  },
 };
 </script>
+
+<style>
+.admin-panel {
+  height: 100%;
+}
+
+.admin-panel-content {
+  overflow-y: scroll;
+}
+</style>
