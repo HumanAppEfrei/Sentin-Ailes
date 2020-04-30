ee903a2ab6f2e2203a310cd3955fae4c3af3cf3d
Camille Briand <camille.briand@efrei.net>
2020-04-30 17:23:58 +0200

    Improve rendering performances of MenuTile component

--- src/components/MenuTile.vue
+++ src/components/MenuTile.vue
@@ -4,12 +4,13 @@
       <v-card-title class="justify-center">
           <v-icon size="75">{{ icon }}</v-icon>
       </v-card-title>
-      <v-card-text class="text-center display-1 font-weight-medium white--text">{{ text }}</v-card-text>
-      <div v-if="notif == 1">
-        <v-chip color="red" ripple=false label="Notification">{{ notif }} notification</v-chip>
-      </div>
-      <div v-else-if="notif > 1">
-        <v-chip color="red" ripple=false label="Notification">{{ notif }} notifications</v-chip>
+      <v-card-text class="text-center display-1 font-weight-medium white--text">
+        {{ text }}
+      </v-card-text>
+      <div v-if="notif >= 1">
+        <v-chip color="red" ripple=false label="Notification">
+          {{ notif }} notification{{ notif === 1 ? '' : 's' }}
+        </v-chip>
       </div>
     </v-card>
   </router-link>
