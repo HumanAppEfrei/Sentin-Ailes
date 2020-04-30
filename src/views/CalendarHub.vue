ceb40757c0363ab962ee00cb2448b8eba798f997
Camille Briand <camille.briand@efrei.net>
2020-04-30 18:34:43 +0200

    Fix max-len in CalendarHub

--- src/views/CalendarHub.vue
+++ src/views/CalendarHub.vue
@@ -2,15 +2,28 @@
   <v-container class="fill-height">
       <v-row justify="center">
           <v-col cols="12" sm="6">
-            <MenuTile color="calendar" icon="event" text="Ajouter un évènement" link="/calendar/event-editor/new"/>
+            <MenuTile
+              color="calendar"
+              icon="event"
+              text="Ajouter un évènement"
+              link="/calendar/event-editor/new" />
           </v-col>
 
           <v-col cols="12" sm="6">
-            <MenuTile color="calendar" icon="event_note" text="Mes évènements programmés" link="/calendar/my-events"/>
+            <MenuTile
+              color="calendar"
+              icon="event_note"
+              text="Mes évènements programmés"
+              link="/calendar/my-events" />
           </v-col>
 
           <v-col cols="12" sm="6">
-            <MenuTile color="calendar" icon="date_range" text="Demandes d'évènement" link="/calendar/event-requests" notif=""/>
+            <MenuTile
+              color="calendar"
+              icon="date_range"
+              text="Demandes d'évènement"
+              link="/calendar/event-requests"
+              notif="" />
           </v-col>
       </v-row>
   </v-container>
